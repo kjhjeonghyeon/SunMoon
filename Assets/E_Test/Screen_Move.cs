b39a1c53a3820b408973a5bd3d9a19a111cec86e
kjhjeonghyeon <80999210+kjhjeonghyeon@users.noreply.github.com>
2023-10-23 17:40:21 +0900

    로비배열

---
+++ Assets/E_Test/Screen_Move.cs
@@ -0,0 +1,19 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class Screen_Move : MonoBehaviour
+{
+    TimelineClip clipL_C;
+    TimelineClip clipR_C;
+    TimelineClip clipC_R;
+    TimelineClip clipC_L;
+
+    private void Start()
+    {
+
+    }
+}
