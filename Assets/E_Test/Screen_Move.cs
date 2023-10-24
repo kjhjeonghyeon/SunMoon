d3d35e5af405d1916212d3428d7ce0e3e9af3e1d
kjhjeonghyeon <80999210+kjhjeonghyeon@users.noreply.github.com>
2023-10-24 09:22:23 +0900

    로비 에딧 기능만 완성

--- Assets/E_Test/Screen_Move.cs
+++ Assets/E_Test/Screen_Move.cs
@@ -1,4 +1,5 @@
 
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,13 +8,36 @@ using UnityEngine.Timeline;
 
 public class Screen_Move : MonoBehaviour
 {
-    TimelineClip clipL_C;
-    TimelineClip clipR_C;
-    TimelineClip clipC_R;
-    TimelineClip clipC_L;
+    CinemachineVirtualCamera v_camera;
+    Vector3 origin;
 
     private void Start()
     {
-
+        v_camera= GameObject.FindAnyObjectByType<CinemachineVirtualCamera>();
+        origin= v_camera.transform.position;
     }
+    private void Update()
+    {
+    }
+public void rightmove()
+    {
+
+        v_camera.transform.position = Vector3.MoveTowards(v_camera.transform.position ,new Vector3(v_camera.transform.position.x+3 , v_camera.transform.position.y, v_camera.transform.position.z),1f);
+    }
+    public void lefttmove()
+    {
+
+        v_camera.transform.position = Vector3.MoveTowards(v_camera.transform.position, new Vector3(v_camera.transform.position.x-3 , v_camera.transform.position.y, v_camera.transform.position.z),1f);
+    }
+    public void Backmove()
+    {
+
+        v_camera.transform.position = Vector3.MoveTowards(v_camera.transform.position, new Vector3(v_camera.transform.position.x , v_camera.transform.position.y, v_camera.transform.position.z+10), 1f);
+    }
+    public void fowardmove()
+    {
+
+        v_camera.transform.position = Vector3.MoveTowards(v_camera.transform.position, new Vector3(v_camera.transform.position.x, v_camera.transform.position.y, v_camera.transform.position.z - 10), 1f);
+    }
+
 }
